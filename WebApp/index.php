c6a02ddf297a19ec941608218fee9f2e98f5f7b6
Zambeeee <113595134+Zambeeee@users.noreply.github.com>
2022-12-21 11:43:06 +0100

    Add files via upload

---
+++ WebApp/index.php
@@ -0,0 +1,21 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home_Page</title>
+</head>
+<body>
+    <p><h1>Welcome</h1></p>
+    <h3>Session in web app.</h3>
+
+    <!-- <a></a> è un hyperlink -->
+    <a href="login.php">Login</a><br>
+    <a href="riservata.php">Vai all'area riservata del sito</a>
+    
+</body>
+</html>
